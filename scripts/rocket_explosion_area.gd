bec1f6beb43df3357652b34335e4d014212ac076
piaggo <44965624+piaggo@users.noreply.github.com>
2024-05-20 12:41:44 +0200

    Fix Rocket Animation

--- scripts/rocket_explosion_area.gd
+++ scripts/rocket_explosion_area.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var gpu_particles_2d = $GPUParticles2D
+
+func _ready():
+	gpu_particles_2d.emitting = true
+	
 
 func _physics_process(_delta):
 	collision_shape_2d.shape.radius += 5
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.4).timeout
 	queue_free()
 
 
