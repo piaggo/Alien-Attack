b26c69684f9ba34407864b759070ec63d3c270a2
piaggo <44965624+piaggo@users.noreply.github.com>
2024-05-21 18:01:29 +0200

    Added Music Volume Slider

--- scripts/controls.gd
+++ scripts/controls.gd
@@ -1,13 +1,25 @@
 extends Control
 
+
+
+@onready var v_slider = $VSlider
+
+@export_enum("Music") var bus_name : String
+
+var bus_index : int = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass  # Replace with function body.
+	set_slider_value()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
+func _on_v_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, linear_to_db(value))
 
-
+func set_slider_value() -> void:
+	v_slider.value = db_to_linear(AudioServer.get_bus_volume_db(1))
